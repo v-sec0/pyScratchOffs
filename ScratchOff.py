70be9619e2639321b22dfb669b9bfd6cf0aaafc4
Levon Watkins Jr <levon@FlatMac-Pro.local>
2026-06-12 23:13:52 -0500

    Increased starting balance

--- ScratchOff.py
+++ ScratchOff.py
@@ -3,89 +3,92 @@ from time import sleep
 
 
 class ScratchOff:
-  def __init__(self, name, price, jackpot) -> None:
-    self.name = name
-    self.price = price
-    self.jackpot = jackpot
+    def __init__(self, name, price, jackpot) -> None:
+        self.name = name
+        self.price = price
+        self.jackpot = jackpot
 
-  def play(self, amount, cash): # Values are assumed to be clean and sanitized
+    def play(self, amount, cash):  # Values are assumed to be clean and sanitized
 
-    # Rolling the odds
-    print("\nScratching card(s)...")
-    sleep(5)
-    earnings = 0
+        # Rolling the odds
+        print("\nScratching card(s)...")
+        sleep(5)
+        earnings = 0
 
-    # Initializing the loop
-    for scratcher in range(amount):
+        # Initializing the loop
+        for scratcher in range(amount):
+            # Roll for jackpot chance to determine if jackpot is eligible
+            jackpot_high = random.randint(0, 100)
+            jackpot_low = random.randint(0, 100)
 
-      # Roll for jackpot chance to determine if jackpot is eligible
-      jackpot_high = random.randint(0,100)
-      jackpot_low = random.randint(0,100)
+            # Establish win-lose brackets and weights for each
+            winning_brackets = ["High Gain", "Mid Gain", "Low Gain"]
+            winning_brackets_weight = [10, 20, 70]
+            losing_brackets = ["High Loss", "Mid Loss", "Some Loss", "Low Loss"]
+            losing_brackets_weight = [92, 5, 2, 1]
 
-      # Establish win-lose brackets and weights for each
-      winning_brackets = ["High Gain", "Mid Gain", "Low Gain"]
-      winning_brackets_weight = [10, 20, 70]
-      losing_brackets = ["High Loss", "Mid Loss", "Some Loss", "Low Loss"]
-      losing_brackets_weight = [92, 5, 2, 1]
+            # Rolling for win-lose bracket
+            win_bracket = random.choices(
+                winning_brackets, weights=winning_brackets_weight, k=1
+            )[0]
+            lose_bracket = random.choices(
+                losing_brackets, weights=losing_brackets_weight, k=1
+            )[0]
 
-      # Rolling for win-lose bracket
-      win_bracket = random.choices(winning_brackets, weights=winning_brackets_weight, k=1)[0]
-      lose_bracket = random.choices(losing_brackets, weights=losing_brackets_weight, k=1)[0]
+            # Checking if jackpot high-low match and result in jackpot chance
+            if jackpot_high != jackpot_low:
+                if win_bracket == "High Gain":
+                    max_gains = self.jackpot * 0.75
+                    min_gains = self.jackpot * 0.50
+                elif win_bracket == "Mid Gain":
+                    max_gains = self.jackpot * 0.50
+                    min_gains = self.jackpot * 0.25
+                else:
+                    max_gains = self.jackpot * 0.25
+                    min_gains = self.jackpot * 0
+            else:
+                if win_bracket == "High Gain":
+                    max_gains = self.jackpot * 1
+                    min_gains = self.jackpot * 0.75
+                elif win_bracket == "Mid Gain":
+                    max_gains = self.jackpot * 0.75
+                    min_gains = self.jackpot * 0.25
+                else:
+                    max_gains = self.jackpot * 0.25
+                    min_gains = self.jackpot * 0
 
-      # Checking if jackpot high-low match and result in jackpot chance
-      if jackpot_high != jackpot_low:
-        if win_bracket == "High Gain":
-          max_gains = (self.jackpot * .75)
-          min_gains = (self.jackpot * .50)
-        elif win_bracket == "Mid Gain":
-          max_gains = (self.jackpot * .50)
-          min_gains = (self.jackpot * .25)
-        else:
-          max_gains = (self.jackpot * .25)
-          min_gains = (self.jackpot * 0)
-      else:
-        if win_bracket == "High Gain":
-          max_gains = (self.jackpot * 1)
-          min_gains = (self.jackpot * .75)
-        elif win_bracket == "Mid Gain":
-          max_gains = (self.jackpot * .75)
-          min_gains = (self.jackpot * .25)
-        else:
-          max_gains = (self.jackpot * .25)
-          min_gains = (self.jackpot * 0)
+            # Working with lose bracket
+            if lose_bracket == "High Loss":
+                max_losses = 100
+                min_losses = 75
+            elif lose_bracket == "Mid Loss":
+                max_losses = 74
+                min_losses = 50
+            elif lose_bracket == "Some Loss":
+                max_losses = 49
+                min_losses = 25
+            else:
+                max_losses = 24
+                min_losses = 0
 
-      # Working with lose bracket
-      if lose_bracket == "High Loss":
-        max_losses = 100
-        min_losses = 75
-      elif lose_bracket == "Mid Loss":
-        max_losses = 74
-        min_losses = 50
-      elif lose_bracket == "Some Loss":
-        max_losses = 49
-        min_losses = 25
-      else:
-        max_losses = 24
-        min_losses = 0
+            # Calculating value won and amount loss
+            amount_won = random.uniform(min_gains, max_gains)
+            percent_loss = random.randint(min_losses, max_losses)
+            actual_won = amount_won - (amount_won * (percent_loss / 100))
 
-      # Calculating value won and amount loss
-      amount_won = random.uniform(min_gains, max_gains)
-      percent_loss = random.randint(min_losses, max_losses)
-      actual_won = amount_won - (amount_won * (percent_loss/100))
+            # print("*" * 20)
+            # print("DEBUG")
+            # print(f"Current Earnings: {earnings}")
+            # print(f"Won: ${amount_won:.2f} |  Loss Percentage: {percent_loss}% | Actual: ${actual_won:.2f}")
+            # print("*" * 20)
 
-      # print("*" * 20)
-      # print("DEBUG")
-      # print(f"Current Earnings: {earnings}")
-      # print(f"Won: ${amount_won:.2f} |  Loss Percentage: {percent_loss}% | Actual: ${actual_won:.2f}")
-      # print("*" * 20)
+            # Making sure value isn't negative
+            if actual_won < 0:
+                actual_won = 0
 
-      # Making sure value isn't negative
-      if actual_won < 0:
-        actual_won = 0
+            # Adding won amount to earnings
+            earnings += actual_won
 
-      # Adding won amount to earnings
-      earnings += actual_won
-
-    print(f"You earned ${earnings:.2f}!\n")
-    sleep(5)
-    return float(earnings + cash)
+        print(f"You earned ${earnings:.2f}!\n")
+        sleep(5)
+        return float(earnings + cash)
--- game.py
+++ game.py
@@ -4,88 +4,97 @@ from ScratchOff import ScratchOff
 low = ScratchOff("Poor Man's Delight", 9.99, 250)
 mid = ScratchOff("Money Multiplier", 19.99, 500)
 high = ScratchOff("Uncle Sam Grand Slam", 49.99, 1000)
-collection = { "1": low, "2": mid, "3": high }
+collection = {"1": low, "2": mid, "3": high}
 
 # Game settings
-starting_balance = 100
+starting_balance = 1000
+
 
 def list_scratchers(cash):
 
-  if cash < collection["1"].price:
-    print(f"\nYou went broke! You were left with ${cash:.2f}!")
-    exit()
+    if cash < collection["1"].price:
+        print(f"\nYou went broke! You were left with ${cash:.2f}!")
+        exit()
 
-  index = 1
-  print("=" * 48)
-  for scratcher in collection:
-    print(f"ID: {index} | Name: {collection[scratcher].name} | Price: ${collection[scratcher].price}")
-    index += 1
-  print("=" * 48)
+    index = 1
+    print("=" * 48)
+    for scratcher in collection:
+        print(
+            f"ID: {index} | Name: {collection[scratcher].name} | Price: ${collection[scratcher].price}"
+        )
+        index += 1
+    print("=" * 48)
+
 
 def select_scratcher(cash):
 
-  print(f"Current Balance: ${cash:.2f}. You have enough to afford the cheapest card!")
+    print(f"Current Balance: ${cash:.2f}. You have enough to afford the cheapest card!")
 
-  selection_valid = False
-  earnings = 0
+    selection_valid = False
+    earnings = 0
 
-  while not selection_valid:
-    try:
-      choice = input("Please select a scratcher via ID (q to quit): ")
+    while not selection_valid:
+        try:
+            choice = input("Please select a scratcher via ID (q to quit): ")
 
-      if choice.lower() == "q":
-        print("Quitting...")
-        exit()
+            if choice.lower() == "q":
+                print("Quitting...")
+                exit()
 
-      if str(choice) not in collection:
-        print("Please make a valid selection!")
-      else:
+            if str(choice) not in collection:
+                print("Please make a valid selection!")
+            else:
+                purchase_valid = False
 
-        purchase_valid = False
+                while not purchase_valid:
+                    card = collection[choice].name
+                    price = collection[choice].price
 
-        while not purchase_valid:
+                    amount = input(
+                        f"How many {card}s would you like to buy? ({cash // price:.0f} max | c to cancel): "
+                    )
 
-          card = collection[choice].name
-          price = collection[choice].price
+                    if amount == "c":
+                        break
 
-          amount = input(f"How many {card}s would you like to buy? ({cash//price:.0f} max | c to cancel): ")
+                    if not amount.isnumeric or int(amount) < 0:
+                        print("Please enter a valid amount!")
+                        continue
 
-          if amount == "c":
-            break
+                    amount = int(amount)
 
-          if not amount.isnumeric or int(amount) < 0:
-            print("Please enter a valid amount!")
-            continue
+                    if (amount * price) > cash:
+                        print("You cannot afford this!")
+                        continue
 
-          amount = int(amount)
+                    cost = amount * price
+                    remaining_balance = cash - cost
 
-          if (amount * price) > cash:
-            print("You cannot afford this!")
-            continue
+                    print(
+                        f"\nYou purchased {amount} card(s) for ${cost:.2f} leaving behind ${remaining_balance:.2f}"
+                    )
+                    purchase_valid = True
+                    selection_valid = True
+                    earnings = collection[choice].play(amount, remaining_balance)
 
-          cost = (amount * price)
-          remaining_balance = cash - cost
+        except ValueError:
+            print("Please enter a valid value!")
 
-          print(f"\nYou purchased {amount} card(s) for ${cost:.2f} leaving behind ${remaining_balance:.2f}")
-          purchase_valid = True
-          selection_valid = True
-          earnings = collection[choice].play(amount, remaining_balance)
+        except KeyboardInterrupt:
+            print("\nQuitting...")
+            exit()
 
-    except ValueError:
-      print("Please enter a valid value!")
+    return earnings
 
-    except KeyboardInterrupt:
-      print("\nQuitting...")
-      exit()
-
-  return earnings
 
 def menu(cash):
-  list_scratchers(cash)
-  earnings = select_scratcher(cash)
-  menu(earnings)
+    list_scratchers(cash)
+    earnings = select_scratcher(cash)
+    menu(earnings)
+
 
 def main():
-  menu(starting_balance)
+    menu(starting_balance)
+
 
 main()
